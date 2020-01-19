c190a9cfe6052c8c3cf66525d0765c1847068393
Maruno <hiro.0405.maru@gmail.com>
2020-01-20 00:00:34 +0900

    OTW

--- app/src/main/java/com/example/worldclock/IndiaFragment.java
+++ app/src/main/java/com/example/worldclock/IndiaFragment.java
@@ -5,9 +5,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 
@@ -20,5 +25,19 @@ public class IndiaFragment extends Fragment {
             Log.e(TAG, "onCreateView", e);
             throw e;
         }
+    }
+
+    private String getToday() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/\nHH/mm/ss", Locale.getDefault());
+        return sdf.format(date);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView japanDate = getView().findViewById(R.id.India_Date);
+        String s = getToday();
+        japanDate.setText(s);
     }
 }
--- app/src/main/java/com/example/worldclock/JapanFragment.java
+++ app/src/main/java/com/example/worldclock/JapanFragment.java
@@ -5,13 +5,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import static android.content.ContentValues.TAG;
 
 public class JapanFragment extends Fragment {
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         try {
@@ -20,5 +26,19 @@ public class JapanFragment extends Fragment {
             Log.e(TAG, "onCreateView", e);
             throw e;
         }
+    }
+
+    private String getToday() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/\nHH/mm/ss", Locale.getDefault());
+        return sdf.format(date);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        TextView japanDate = getView().findViewById(R.id.Japan_Date);
+        String s = getToday();
+        japanDate.setText(s);
     }
 }
